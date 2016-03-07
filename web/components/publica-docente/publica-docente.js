e68c24d14c38ac3f65fe4e7b2c81282adc7f1c93
nogayo <nogayo.jpm@gmail.com>
2016-03-07 00:15:26 -0400

    agregando un textArea a la parte de publicacion de aviso de parte del docente

---
+++ web/components/publica-docente/publica-docente.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('app.publicaDocente', [])
+           .controller('PublicaDocenteController', PublicaDocenteController);
+
+    function PublicaDocenteController() {
+        var vm = this;
+    }
+})();
