0099d39a7fa5a7edfecdf86eb475915dbedf4ea2
Arolov <ariel20091998@gmail.com>
2016-03-14 18:45:05 -0400

    avisos y publicacion de avisos separado

--- web/app.js
+++ web/app.js
@@ -9,6 +9,7 @@
         'app.users',
         'app.tasks',
         'app.avisos',
+        'app.publicar',
         'app.info'
     ];
     
@@ -31,6 +32,9 @@
             },
             {
                 path: '/avisos', component: 'avisos'
+            },
+            {
+                path: '/publicar', component: 'publicar'  
             },
             {
                 path: '/info', component: 'info'
